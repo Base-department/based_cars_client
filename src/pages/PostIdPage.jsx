add93eb37291809260a122b072fb93a37de88b9f
Syunsha <a1eksey.bru@yandex.ru>
2022-09-18 23:45:39 +0300

    PostIdPage

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -5,12 +5,13 @@ import PostService from "../API/PostService";
 import { useFetching } from "../hooks/useFetching";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import MyButton from "../components/UI/button/MyButton";
+import MyInput from "../components/UI/input/input";
 
 const PostIdPages = () => {
   const router = useNavigate();
   const params = useParams();
   const [post, setPost] = useState({});
-  const [comments, setComments] = useState([]);
 
   const [fetchPostById, isLoading, error] = useFetching(async (id) => {
     const response = await PostService.getById(id);
@@ -21,14 +22,8 @@ const PostIdPages = () => {
     const response = await PostService.deleteById(id);
   });
 
-  const [fetchComments, isComLoading, comErorr] = useFetching(async (id) => {
-    const response = await PostService.getCommentsByPostId(params.id);
-    setComments(response.data);
-  });
-
   useEffect(() => {
     fetchPostById(params.id);
-    fetchComments(params.id);
     // delPostById(params.id);
   }, []);
   return (
@@ -37,45 +32,28 @@ const PostIdPages = () => {
         <h1>Вы открыли страницу автомобиля с Id = {params.id}</h1>
         <form>
           <fieldset>
-            <legend>Id</legend>
-            <input type="text" />
+            <form>
+              <legend>Id авто</legend>
+              <MyInput value={params.id} type="text" placeholder="Id авто" />
+              <legend>Модель авто</legend>
+              <MyInput type="text" placeholder="Модель авто" />
+              <legend>Владелец</legend>
+              <MyInput type="text" placeholder="Владелец" />
+              <legend>Пробег</legend>
+              <MyInput type="text" placeholder="Пробег" />
+              <MyButton>Изменить</MyButton>
+              <MyButton>Сохранить</MyButton>
+            </form>
           </fieldset>
-          <fieldset>
-            <legend>Модель</legend>
-            <input type="text" />
-          </fieldset>
-          <fieldset>
-            <legend>Владелец</legend>
-            <input type="text" />
-          </fieldset>
-          <fieldset>
-            <legend>Пробег</legend>
-            <input type="text" />
-          </fieldset>
-          <button>Изменить</button>
-          <button>Сохранить</button>
           <button
-            onClick={async () => {
-              const response = await PostService.deleteById(params.id);
+            onClick={() => {
+              const response = PostService.deleteById(params.id);
               router(`/posts`);
             }}
           >
             Удалить
           </button>
         </form>
-        {/* <div>
-          {post.id}. {post.title}{" "}
-        </div> */}
-        {/* <h1> */}
-        {/* <div>
-          {comments.map((comm) => (
-            <div style={{ marginTop: 15 }}>
-              <h5>{comm.email}</h5>
-              <div>{comm.body}</div>
-            </div>
-          ))}
-        </div> */}
-        {/* </h1> */}
       </div>
     </center>
   );
